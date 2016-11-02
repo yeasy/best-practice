30c871471170b65b776b93adfee9ebe72aed337c
Baohua Yang <yangbaohua@gmail.com>
2016-11-02 12:16:26 +0800

    Fix project structure

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -4,18 +4,17 @@ const webpack = require('webpack');
 
 module.exports = {
   entry: {
-        app: path.resolve(__dirname, 'src/entry.js'),
-        vendors: ['react'] 
+        app: path.resolve(__dirname, 'src/js/index.js')
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist/js'),
     filename: "bundle.js"
   },
   module: {
     loaders: [
       { test: /\.css$/, loader: "style!css"},
       { test: /\.js$/, exclude: /node_modules/, loader: "babel" },
-      { test: /\.jsx?$/, loader: 'babel' },
+      { test: /\.jsx?$/, loader: "babel" },
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
       { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
     ]
