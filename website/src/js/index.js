4b134a64932e5317ca79e68639e4124e0a730c8f
Baohua Yang <yangbaohua@gmail.com>
2016-11-02 16:49:47 +0800

    Add more test

--- website/src/js/index.js
+++ website/src/js/index.js
@@ -23,3 +23,22 @@ ReactDOM.render(
   element,
   document.getElementById('root')
 );
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('list')
+);
